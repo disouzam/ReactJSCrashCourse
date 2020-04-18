2c076f229596407cf53ad3dd426b447448789795
Dickson Alves de Souza <dickson.souza@rhimagnesita.com>
2020-04-17 23:56:34 -0300

    Lessons paused at 24 minutes and 39 seconds

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed:false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed:false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed:false
+      }
+    ]
+  }  
+  render(){
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+        <Todos></Todos>
+      </div>
+    );
+  }
 }
 
 export default App;
